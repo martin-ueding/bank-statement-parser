cffcd71f56570971be041b570800b36ab9e080b2
Martin Ueding <dev@martin-ueding.de>
2014-02-09 14:56:16 +0100

    Renerate some plot

--- bankstatement/__init__.py
+++ bankstatement/__init__.py
@@ -5,18 +5,16 @@
 
 import argparse
 import csv
-import sys
+import matplotlib.pyplot as pl
+import numpy as np
+import prettytable
 
 import bankstatement.expense
 
-
 __docformat__ = 'restructuredtext en'
-
 
 def main():
     options = _parse_args()
-
-    print(sys.path)
 
     expenses = []
 
@@ -27,9 +25,37 @@ def main():
             for row in reader:
                 expenses.append(bankstatement.expense.Expense(*row))
 
-    for expense_ in expenses:
-        print(expense_)
+    report_data = []
+    t = prettytable.PrettyTable(['Date', 'Category', 'Store', 'Amount'])
+    t.align = 'l'
+    for expense in expenses:
+        if expense.amount < 0 and expense.store.title is not None:
+            row = [expense.date, expense.store.category, expense.store.title, '{:+7.2f} €'.format(expense.amount)]
+            t.add_row(row)
+            report_data.append(expense)
+    print(t)
 
+    report_categories(report_data)
+
+def report_categories(expenses):
+    total = sum([expense.amount for expense in expenses])
+    cs = {}
+    for expense in expenses:
+        if not expense.store.category in cs:
+            cs[expense.store.category] = 0.0
+        cs[expense.store.category] += expense.amount
+
+    labels, sub_totals = zip(*cs.items())
+    fractions = np.array(sub_totals) / total
+
+    fig = pl.figure(1, figsize=(15,15))
+    #ax = fig.axes([0.1, 0.1, 0.8, 0.8])
+    ax = fig.add_subplot(1, 1, 1)
+
+    labels = ['{} | {:.2f} € | {:.0f}%'.format(x, y, z*100) for x, y, z in zip(labels, sub_totals, fractions)]
+
+    ax.pie(fractions, labels=labels)
+    fig.savefig('report_categories.pdf')
 
 def _parse_args():
     '''
@@ -44,7 +70,6 @@ def _parse_args():
     #parser.add_argument('--version', action='version', version='<the version>')
 
     return parser.parse_args()
-
 
 if __name__ == '__main__':
     main()
