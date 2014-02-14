5b09e43059e66db6a6420ce40c549b9c7bb860ba
Martin Ueding <dev@martin-ueding.de>
2014-02-14 13:26:00 +0100

    Delete all the code

--- bankstatement/__init__.py
+++ bankstatement/__init__.py
@@ -9,53 +9,11 @@ import matplotlib.pyplot as pl
 import numpy as np
 import prettytable
 
-import bankstatement.expense
-
 __docformat__ = 'restructuredtext en'
 
 def main():
     options = _parse_args()
 
-    expenses = []
-
-    for statement in options.statements:
-        with open(statement) as f:
-            reader = csv.reader(f, delimiter=options.delimiter)
-            next(reader)
-            for row in reader:
-                expenses.append(bankstatement.expense.Expense(*row))
-
-    report_data = []
-    t = prettytable.PrettyTable(['Date', 'Category', 'Store', 'Amount'])
-    t.align = 'l'
-    for expense in expenses:
-        if expense.amount < 0 and expense.store.title is not None:
-            row = [expense.date, expense.store.category, expense.store.title, '{:+7.2f} €'.format(expense.amount)]
-            t.add_row(row)
-            report_data.append(expense)
-    print(t)
-
-    report_categories(report_data)
-
-def report_categories(expenses):
-    total = sum([expense.amount for expense in expenses])
-    cs = {}
-    for expense in expenses:
-        if not expense.store.category in cs:
-            cs[expense.store.category] = 0.0
-        cs[expense.store.category] += expense.amount
-
-    labels, sub_totals = zip(*cs.items())
-    fractions = np.array(sub_totals) / total
-
-    fig = pl.figure(1, figsize=(15,15))
-    #ax = fig.axes([0.1, 0.1, 0.8, 0.8])
-    ax = fig.add_subplot(1, 1, 1)
-
-    labels = ['{} | {:.2f} € | {:.0f}%'.format(x, y, z*100) for x, y, z in zip(labels, sub_totals, fractions)]
-
-    ax.pie(fractions, labels=labels)
-    fig.savefig('report_categories.pdf')
 
 def _parse_args():
     '''
@@ -65,8 +23,9 @@ def _parse_args():
     :rtype: Namespace
     '''
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument('statements', metavar='statement.csv', type=str, nargs='+', help='Bank statements in CSV CAMT format.')
-    parser.add_argument('--delimiter', default=';', help='CSV delimiter. Default %(default)s.')
+    parser.add_argument('action', type=str)
+    parser.add_argument('what', type=str)
+    #parser.add_argument('--delimiter', default=';', help='CSV delimiter. Default %(default)s.')
     #parser.add_argument('--version', action='version', version='<the version>')
 
     return parser.parse_args()
--- doc/index.rst
+++ doc/index.rst
@@ -1,22 +1,19 @@
-.. bank-statement-parser documentation master file, created by
-   sphinx-quickstart on Fri Feb 14 13:09:26 2014.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+..  Copyright © 2014 Martin Ueding <dev@martin-ueding.de>
+    Licensed under The GNU Public License Version 2 (or later)
 
-Welcome to bank-statement-parser's documentation!
-=================================================
+bank-statement-parser's documentation
+=====================================
 
-Contents:
 
 .. toctree::
-   :maxdepth: 2
+    :maxdepth: 2
+    :glob:
 
-
-
+    *
+    
 Indices and tables
 ==================
 
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
-
+- :ref:`genindex`
+- :ref:`modindex`
+- :ref:`search`
